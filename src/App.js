03dc25470f4f7b0ae5c2673fbf61d4c8ceae971e
eriysd <74118903+eriysd@users.noreply.github.com>
2021-07-18 17:39:42 -0400

    added a button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button
+          startIcon={<SaceIcon />}
+          size="large"
+          href="#"
+          variant="contained"
+          color="primary"
+          style={{ fontSize: 20 }}
+        >
+          Hello World
+        </Button>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
