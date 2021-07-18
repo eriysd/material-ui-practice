09d44bcdda67eaf1e5e6c6d467e4c2cd97248faa
eriysd <74118903+eriysd@users.noreply.github.com>
2021-07-18 17:57:45 -0400

    added checkbox

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
 import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Checkbox from "@material-ui/core/Checkbox";
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true);
+  return (
+    <div>
+      <Checkbox
+        checked={checked}
+        icon={<SaveIcon />}
+        checkedIcon={<DeleteIcon />}
+        onChange={(e) => setChecked(e.target.checked)}
+        label="Test"
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button
-          startIcon={<SaceIcon />}
-          size="large"
-          href="#"
-          variant="contained"
-          color="primary"
-          style={{ fontSize: 20 }}
-        >
-          Hello World
-        </Button>
+        <CheckboxExample />
+        <ButtonGroup variant="contained" size="large">
+          <Button
+            startIcon={<SaveIcon />}
+            href="#"
+            color="primary"
+            style={{ fontSize: 20 }}
+          >
+            Save
+          </Button>
+          <Button
+            endIcon={<DeleteIcon />}
+            href="#"
+            color="secondary"
+            style={{ fontSize: 20 }}
+          >
+            Discard
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
